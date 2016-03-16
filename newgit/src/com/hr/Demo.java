824ffb7c6caa8a8b08d20c34fb8bc08b0480f888
rhao <394525117@qq.com>
2016-03-16 17:10:22 +0800

    s

---
+++ newgit/src/com/hr/Demo.java
@@ -0,0 +1,16 @@
+package com.hr;
+
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println();
+	}
+
+	
+	public static void aa(String[] args) {
+		System.out.println("ddddd");
+	}
+}
